128ab8f0b96933daff3e09843f1d5814befa4daf
Dina Spencer <dkay22@gmail.com>
2022-09-16 15:52:27 +0200

    Create registration button on login view
    (Not yet linked to registration view)

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -15,6 +15,9 @@ export function LoginView(props) {
             <label>Username: <input type="text" value={username} onChange={e => setUsername(e.target.value)} /></label>
             <label>Password: <input type="password"  value={password} onChange={e => setPassword(e.target.value)} /></label>
             <button type="submit" onClick={handleSubmit}>Submit</button>
+         
+            <button type="button" onClick={handleSubmit}>Register</button>
         </form>
+         
     );
 }
