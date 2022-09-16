b19b1076a9dc56b817f4f17e585e6d574e0e067b
Dina Spencer <dkay22@gmail.com>
2022-09-16 11:09:25 +0200

    Add login view, registration view, implement Hooks

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import axios from "axios";
+
+//import views
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
  
@@ -7,13 +11,22 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                {_id: 1, Title: 'Knocked Up', Description: 'Knocked Up is a 2007 American romantic comedy film written, co-produced and directed by Judd Apatow, and starring Seth Rogen, Katherine Heigl, Paul Rudd, and Leslie Mann. It follows the repercussions of a drunken one-night stand between a slacker and a recently promoted media personality that results in an unintended pregnancy.', ImagePath: 'http://www.freemovieposters.net/posters/knocked_up_2007_727_poster.jpg'},
-                {_id: 2, Title: 'Amelie', Description: 'Amélie, also known as The Fabulous Destiny of Amélie Poulain, is a 2001 French-language romantic comedy film directed by Jean-Pierre Jeunet. The film is a whimsical depiction of contemporary Parisian life, set in Montmartre. It tells the story of a shy waitress, played by Audrey Tautou, who decides to change the lives of those around her for the better while dealing with her own isolation.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BNDg4NjM1YjMtYmNhZC00MjM0LWFiZmYtNGY1YjA3MzZmODc5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg'},
-                {_id: 3, Title: 'Eternal Sunshine of the Spotless Mind', Description: 'Eternal Sunshine of the Spotless Mind is a 2004 American romantic science fiction drama film written by Charlie Kaufman, directed by Michel Gondry, and starring Jim Carrey and Kate Winslet. Pierre Bismuth created the story with Kaufman and Gondry. The film features an ensemble supporting cast that includes Kirsten Dunst, Mark Ruffalo, Elijah Wood, and Tom Wilkinson. The title of the film is a quotation from the 1717 poem Eloisa to Abelard by Alexander Pope. The picture uses elements of psychological drama, science fiction and a nonlinear narrative to explore the nature of memory and romantic love.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BZTg3ODg2MzMtZmRmYy00ZWUwLTk5Y2QtOThmOTY1ZWZjZmJlXkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg'}
-            ],
-            selectedMovie: null
+            movies: [],
+            selectedMovie: null,
+            user: null
         }
+    }
+
+    componentDidMount() {
+        axios.get('https://dinaspencer-myflix.herokuapp.com/movies')
+        .then(response => {
+            this.setState({
+                movies: response.data
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -22,9 +35,20 @@ export class MainView extends React.Component {
         });
     }
 
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
     render() {
-        const {movies, selectedMovie} = this.state;
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+        const {movies, selectedMovie, user} = this.state;
+        
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+        
+        if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
@@ -35,4 +59,5 @@ export class MainView extends React.Component {
             </div>
         );
     }
+ 
 }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export class MovieView extends React.Component {
+
     render() {
        const {movie, onBackClick} = this.props;
        return (
@@ -15,6 +16,23 @@ export class MovieView extends React.Component {
             <div className="movie-description">
                 <span className="label">Description: </span>
                 <span className="value">{movie.Description}</span>
+            </div>
+            <div className="movie-genre">
+                <span className="label">Genre: </span>
+                <span className="value">{movie.Genre.Name}</span>
+                <br></br>
+                <span className="label">Description: </span>
+                <span className="value">{movie.Genre.Description}</span>
+            </div>
+            <div className="movie-director">
+                <span className="label">Director: </span>
+                <span className="value">{movie.Director.Name}</span>
+                <br></br>
+                <span className="label">Bio: </span>
+                <span className="value">{movie.Director.Bio}</span>
+                <br></br>
+                <span className="label">Birth: </span>
+                <span className="value">{movie.Director.Birth}</span>
             </div>
             <button onClick={() => {onBackClick(null);}}>Back</button>
         </div>
